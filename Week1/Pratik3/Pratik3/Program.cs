b404e3a38dbacb237d28b4410433e805d5bc043d
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-09-30 13:51:26 +0300

    pratik3 added

---
+++ Week1/Pratik3/Pratik3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik3
+{
+    //Pratik3 If / Else
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=0;
+            string numDurum = "a Esittir";
+            Console.Write("Lütfen bir sayı Giriniz:");
+            int.TryParse(Console.ReadLine(),out num);
+            if (num != 10)//Sayı eşit ise iki kontrol yapmaya gerek olmasın diye esiitlik bakıyoruz
+            {
+                numDurum = num > 10 ? "dan Büyüktür" : "dan Küçüktür";//  if else tanımı yerine kontrol edip degerin türünü alıyoruz
+            }
+            Console.WriteLine($"Girilen sayı 10'{numDurum} ");//Tekrar yazdıyıoruzz
+            numDurum = num%2==0 ? "çifttir" : "tektir";//sayının hangi durumda oldugunu büyük,kücük,esitlik tekrar degiştiriyiroz ve  tek çift yapıyoruz
+            Console.WriteLine($"Girilen sayı {numDurum}");
+
+            Console.ReadLine();
+        }
+    }
+}
