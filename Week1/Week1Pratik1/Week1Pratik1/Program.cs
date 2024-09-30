0360bd1757b126a1f64d63aad28cd36eabf53b11
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-09-30 09:51:00 +0300

    Week1-Pratik1
    Pratik1(Pratik - İlk Uygulamam) v1 added

---
+++ Week1/Week1Pratik1/Week1Pratik1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Week1Pratik1
+{
+	//Current  Pratik1(Pratik - İlk Uygulamam)
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Atakan GÜRDAL");
+			Console.WriteLine("Patikanın kaliteli egitim içerikleri ve geniş agının sagladıgı imkanlarını kullanarak kendimi geliştirmek için bootcampteyim ");
+			
+			Console.ReadKey(true);
+		}
+	}
+}
