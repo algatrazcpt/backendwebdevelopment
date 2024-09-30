d33fbcf60a237fa7d62c3a7f79b3dbcc9bdf3644
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-09-30 15:47:59 +0300

    pratik4 added

---
+++ Week1/Pratik4/Pratik4/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik4
+{
+    internal class Program
+    {
+        //Pratik 4  If / Else ve Switch-Case
+        static void Main(string[] args)
+        {
+            int apple = 2;
+            int pine = 3;
+            int straw = 2;
+            int banana = 3;
+            int other = 4;
+
+            Console.WriteLine("Rüya Manavına Hoşgeldiniz");
+            Console.WriteLine($"Elma={apple}");
+            Console.WriteLine($"Armut={pine}");
+            Console.WriteLine($"Çilek={straw}");
+            Console.WriteLine($"Muz={banana}");
+            Console.WriteLine($"Diger bütün meyveler={other}");
+            Console.WriteLine();
+            Console.Write("Hangi meyveyi satın almak istersiniz? (Elma/Armut/Çilek/Muz/Diger):");
+
+            string currentFruits=Console.ReadLine().ToLower();//Kullanıcıdan aldıgımız meyveyi direkmen küçük harfelere çeviriyoruz
+
+            int currentFruitsPrice= other;// ilk deger  olarak diger meyvelerin parasını temsil ediyoruz ve ifelse yapısına eklemiyoruz
+
+            if(currentFruits=="elma")
+            {
+                currentFruitsPrice = apple;
+            }
+            else if (currentFruits == "armut")
+            {
+                currentFruitsPrice = pine;
+            }
+            else if (currentFruits == "çilek")
+            {
+                currentFruitsPrice = straw;
+            }
+            else if (currentFruits == "muz")
+            {
+                currentFruitsPrice = banana;
+            }
+            Console.WriteLine($"Seçtiginiz meyvenin fiyatı:{currentFruitsPrice} TL");//Degeri if içinde olamdan yazdırıyoruz
+            Console.ReadLine();
+            //Switch Case version 
+            Console.WriteLine("Switch Version");
+            switch (currentFruits)//if else yapısının switch yapısı olarak tekrar yazılışı
+            {
+                case "elma":
+                {
+                        currentFruitsPrice = apple;
+                        break;
+                }
+                case "armut":
+                {
+                        currentFruitsPrice = pine;
+                        break;
+                }
+                case "çilek":
+                {
+                        currentFruitsPrice = straw;
+                        break;
+                }
+                case "muz":
+                {
+                        currentFruitsPrice = banana;
+                        break;
+                }
+                default://son atamayı other yapıyoruz
+                { 
+                        currentFruitsPrice =other ;
+                        break;
+                }
+
+            }
+            Console.WriteLine($"Seçtiginiz meyvenin fiyatı:{currentFruitsPrice} TL");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Week1/Pratik4/README.md
@@ -0,0 +1,21 @@
+# Pratik4
+## Sorunun Tanımı
+Bir Manav Uygulaması Geliştiriyoruz.
+
+Rüya manavında fiyatlar çok uygun!
+
+Elma = 2 TL
+
+Armut = 3 TL
+
+Çilek = 2 TL
+
+Muz = 3 TL
+
+Diğer bütün meyveler = 4 TL
+
+Kullanıcımıza konsol ekranında hangi meyveyi satın almak istediğini soruyoruz. Aldığımız cevaba göre ilgili meyvenin fiyatını ekrana yazdırıyoruz. 
+
+Araştıralım -> Büyük küçük harf duyarlılığını ortadan kaldırabiliriz. bunun için string metotları olan ToUpper / ToLower kullanabiliriz. Yapay zekadan yardım alabilirsiniz!
+
+Ek-> Bu uygulamayı hem If hem de Switch karar yapıları ile yapmanızı bekliyoruz. Ardından yorum satırı olarak sizce bu tür bir problemi hangi kontrol yapısıyla çözmenin doğru oluduğunu ve nedenini açıklamanızı bekliyoruz.
