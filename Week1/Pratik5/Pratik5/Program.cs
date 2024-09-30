2e44114b447204d9bdcc8aa2eb67f89864fe1618
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-09-30 17:17:25 +0300

    pratik5 added

---
+++ Week1/Pratik5/Pratik5/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik5
+{
+    internal class Program
+    {
+        // Pratik 5 Kim Milyoner Olmak İster ?
+        static void Main(string[] args)
+        {
+            //Temel degiskenleri tanımlayıop  ilk degerlerini tanımlıyorum
+            int maxFaultCount = 2;
+            int maxQuestion = 3;
+            int currentQuestion=1;
+            int currentTrueQuestion =0;
+            bool gamePlayAble = true;
+
+
+            //İlk Soru tanımlama
+            string question1 = "Şıklardan dogru yazımlı olanı seçin";
+            string answer1 = "int num=10%2==0 ? 100 : 0";
+            string answer2 = "int num=10%2=0 : 100 ? 0";
+            char correctAnswer = 'A';
+
+
+            //Şartlı döngü ile istedigim kadar çalışmasını saglıyorum
+            while (gamePlayAble)
+            {
+                    //Soruları yazdırıyoruz
+                    Console.WriteLine($"{currentQuestion})Soru {question1}");
+                    Console.Write($"A){answer1} B){answer2} \n");
+                    Console.Write("Cevabınız:");
+                    char currentAnswer = char.Parse(Console.ReadLine());
+
+
+                    //Dogru cevaplar yerine yanlıs cevapları kontrol ediyorum
+                    if (Char.ToLower(currentAnswer) != Char.ToLower(correctAnswer))
+                    {
+                        //Yanlıs sayısını azaltıp devam edebilecek hakkı kalmadıgını kontrol ediyoruz
+                        maxFaultCount--;
+
+                        if (maxFaultCount < 1)
+                        {
+                            Console.WriteLine("Oyunu kaybettiniz");
+                            gamePlayAble = false;
+                            Console.ReadLine();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{maxFaultCount} yanlış hakkınız kaldı");
+                        }
+                    }
+                    else
+                    {
+                        if (maxQuestion >=currentQuestion)
+                        {
+                            Console.WriteLine(maxQuestion > currentQuestion ? "Yeni Sorunuz" : "Son Soru");
+                        //dogru sayısını artırıyoruz
+                         currentTrueQuestion++;
+                        }
+                    }
+                //islemlerden sonrası soru sayını artırıyorum 
+                currentQuestion++;
+                //Yeni Soruları swicth için de tutup tanımlıyorum
+                switch (currentQuestion)
+                    {
+                        case 2:
+                            {
+                            //Yeni degiskenler olusturmadan degsikenleri düzenleyip tekrar kullanıyorum
+                                question1 = " Switch case kullanımında break kullanımı zorunlumudur ?";
+                                answer1 = "Zorunludur";
+                                answer2 = "Zorunlu degildir";
+                                correctAnswer = 'A';
+                                break;
+                            }
+                        case 3:
+                            {
+                                question1 = "float veri tipi olarak ondalıklı sayı alamaz?";
+                                answer1 = "Dogru";
+                                answer2 = "Yanlış";
+                                correctAnswer = 'B';
+                                break;
+                            }
+                     default: //Oyun bitimini defaul ile baglantılı yapıyoruz
+                        {
+                            Console.WriteLine("Tebrikler yarışmayı kazandınız");
+                            Console.WriteLine($"Toplam {currentTrueQuestion} dogrunuz var");
+                            gamePlayAble = false;//While döngüsünün daha fazla devam etmesine gerek kalmadıgı için false yapıyoruz
+                            Console.ReadLine();
+                            break;
+                        }
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Week1/Pratik5/README.md
@@ -0,0 +1,15 @@
+# Pratik
+## Sorunun Tanımı
+Bir bilgi yarışması yapıyoruz!
+
+2 cevap şıklı 3 adet sorumuz var !
+
+Yarışmacı 3 sorudan 2'sini doğru cevaplar  ise 1 Milyon TL'lik büyük ödülü kazanacak!
+
+Bu uygulamada kullanıcının doğru cevaplarını saymak için bir değişkene ihtiyacımız olduğunu unutmayın !
+
+Akış -> Kullanıcıya birinci soruyu soruyoruz ve cevabı alıyoruz. ( A ya da B şeklinde - Büyük küçük harf duyarlılığı olmasın!) , Ardından cevabın doğruluk durumuna göre bir mesaj ile kullanıcıya durumu bildiriyoruz. 
+
+İlk 2 soruya cevap veren kullanıcının büyük ödülü kazanma ihtimali ortadan kalktığı için 3. soruyu sormuyoruz.
+
+Final neticesinde kullanıcıya büyük ödülü kazanıp kazanmadığını bir konsol mesajıyla bildiriyoruz.
