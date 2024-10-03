a4e62fbd25c22c3c1f628574274cec74680a6b60
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-10-03 09:34:07 +0300

    Dowhile while difference

---
+++ Week2/Pratik3DowhileDoDiffrence/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Pratik3DowhileDoDiffrence
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int counter = 0;
+            Console.Write("Bir sayı giriniz:");
+            int num=int.Parse(Console.ReadLine());
+
+            //döngüdeki koşul saglanmasada birr defa her zamaan çalışır
+            Console.WriteLine("DoWhile Döngüsü");
+            do
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                counter++;
+            }
+            while ( counter<num);
+            //
+            //
+            //
+            //döngüdeki koşul saglanmasaı zorunludur
+            counter = 0;
+            Console.WriteLine("While Döngüsü");
+            while( counter<num)
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                counter++;
+            }
+            Console.ReadLine();
+
+            //koşul saglanmadıgı durumda do while bir kere çalışır while döngüsünde böyle bir durum olamaz
+        }
+    }
+}
---
+++ Week2/Pratik3DowhileDoDiffrence/README.md
@@ -0,0 +1,8 @@
+# Pratik3 - DoWhile  While döngüsü farkları
+## Sorunun tanımı
+
+Kullanıcının girdiği bir limit değerine kadar 0'dan bir sayaç çalıştırıp artırarak, ekrana " Ben bir Patika'lıyım " yazdırır.
+
+Örnek inputlar : 10 ve -5
+
+Uygulama testi sonrası while ve do-while arasındaki farkı açıklar.
