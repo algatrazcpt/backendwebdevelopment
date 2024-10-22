f35381dd85778e9bfef90545ffb2593b5ba32daf
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-10-22 19:43:56 +0300

    pratik7 added

---
+++ Week4/Pratik7Absraction/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Pratik7Absraction
+{
+    public abstract class People
+    {
+        string name="Belirtmemiş";
+        string surName="Belirtmemiş";
+        string departmanName= "Belirtmemiş";
+
+        public string Name { get => name; set => name = value; }
+        public string SurName { get => surName; set => surName = value; }
+        public string DepartmanName { get => departmanName; set => departmanName = value; }
+        public string ShowInfo() => $"Id bilgiler[ad={name} soyad={surName} departman={departmanName} ]";
+        //Sınıfı soyut yaptık diger sınıflarda kullanabilmek için
+        public abstract void Gorev();
+
+    }
+    public class ProjectManager : People
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine(ShowInfo()+ " Proje yöneticisi olarak çalışıyorum ");
+        }
+    }
+    public class Developer : People
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine(ShowInfo() + " Geliştirici  olarak çalışıyorum ");
+        }
+    }
+    public class Tester : People
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine(ShowInfo() + " Tester olarak  çalışıyorum ");
+        }
+    }
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Şirket çalışanlarımız");
+
+            //burda kalıtım yapılan sınıftan bir liste yaptım
+            List<People> departmen = new List<People>();
+
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Name = "Atakan";
+            projectManager.DepartmanName = "Proje Yöneticisi";
+
+            Developer developer = new Developer();
+            developer.Name = "Developer1";
+            developer.SurName = "Developer1 surname";
+            developer.DepartmanName = "DeveloperYöneticisi";
+
+            Tester tester = new Tester();
+            tester.Name = "Tester1";
+            tester.SurName = "Tester1 surname";
+            tester.DepartmanName = "DeveloperYöneticisi";
+            //kalıtımdan türettiklerimizi kalıtımlı sınıfa ekleyebiliriz
+            departmen.Add(projectManager);
+            departmen.Add(developer);
+            departmen.Add(tester);
+
+            foreach(People a in departmen)
+            {
+                a.Gorev();
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Week4/Pratik7Absraction/Readme.md
@@ -0,0 +1,17 @@
+# Pratik7 - Absraction
+
+## Ýçerikler
+
+Düþünün ki bir þirketin çalýþanlarýný temsil eden bir program yazýyorsunuz.
+
+Her çalýþan için ad, soyad, departman gibi ortak özellikler bulunmaktadýr
+
+Ancak her çalýþanýn görevi (pozisyonu) farklý olabilir (Örneðin, Yazýlým Geliþtirici, Proje Yöneticisi, Satýþ Temsilcisi).
+
+Gorev metodunu çaðýrdýðýmýzda her çalýþan kendi yaptýðý iþi konsol ekranýnda söyleyecek.
+
+Örneðin
+
+Hasan Çýldýrmýþ þirketin Proje yönetcisi, kendisi üzerinden Gorev() abstract metodu çaðýrýldýðýnda konsol ekranýnda
+
+" Proje yöneticisi olarak çalýþýyorum. " yazdýrýlmasýný istiyoruz.
