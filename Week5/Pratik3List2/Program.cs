4d23e651cca03444f32f5ed72c036e1c6d3b06bc
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-10-27 18:23:02 +0300

    pratik3 list2 added

---
+++ Week5/Pratik3List2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Pratik3List2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int coffeCount = 5;
+            Console.WriteLine($"{coffeCount} tane kahve giriniz");
+            List<string> coffeList = new List<string>();
+            for (int index = 0; index < coffeCount; index++) 
+            {
+                Console.Write($"Kahve {index+1}:");
+                //listeye ekleme ile gelen verileri aktarıyorum
+                coffeList.Add(Console.ReadLine());
+            }
+            Console.WriteLine("Kahve Listesi");
+            foreach (string coffe in coffeList)
+            {
+                Console.WriteLine(coffe);
+            }
+        }
+    }
+}
---
+++ Week5/Pratik3List2/Readme.md
@@ -0,0 +1,6 @@
+# Pratik3 - Lists2
+
+## Ýçerikler
+
+kullanýcýnýn konsoldan gireceði 5 kahve ismiyle bir liste oluþturup, bu listeyi foreach döngüsüyle yazdýrýr
+
