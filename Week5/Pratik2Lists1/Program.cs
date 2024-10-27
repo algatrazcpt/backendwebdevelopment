f4299fdd64f5e8fc62e84a753083cbf5467d244f
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-10-27 18:12:32 +0300

    list1 added

--- Week5/Pratik2Lists1/Program.cs
+++ Week5/Pratik2Lists1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> infulanceList;
+            //Bütün ünlüleri tek bir strinte tuttum
+            string infulances= "Bülent Ersoy,Ajda Pekkan,Ebru Gündeş,Hadise,Hande Yener,Tarkan,Funda Arar,Demet Akalın";
+            //ünlüleri virgüle göre böldürüp diziyi Generice çevirdim
+            infulanceList = new List<string>(infulances.Split(","));
+            Console.WriteLine("** Davetliler **");
+            for (int index=0;index<infulanceList.Count;index++)
+            {
+                Console.WriteLine($"{index+1} - {infulanceList[index]}");
+            }
+
         }
     }
 }
---
+++ Week5/Pratik2Lists1/Readme.md
@@ -0,0 +1,6 @@
+# Pratik2 - List1
+
+## Ýçerikler
+
+Önceden oluþturulmuþ 10 tane kiþiyi foreach ile ekrana yazdýrýr
+
