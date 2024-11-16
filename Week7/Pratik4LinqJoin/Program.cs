beed3e97c6c66ff9b6691d2ee2eb2ee18a8168d3
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-11-16 13:08:07 +0300

    Pratik4LinqJoin

---
+++ Week7/Pratik4LinqJoin/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+
+namespace Pratik4LinqJoin
+{
+    public class Author
+    {
+        public int authorId;
+        public string authorName;
+
+        public Author(int authorId, string authorName)
+        {
+            this.authorId = authorId;
+            this.authorName = authorName;
+        }
+    }
+    public class Book
+    {
+        public int authorId;
+        public int bookId;
+        public string bookTitle;
+
+        public Book(int authorId, int bookId, string bookTitle)
+        {
+            this.authorId = authorId;
+            this.bookId = bookId;
+            this.bookTitle = bookTitle;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Author> authorsList=new List<Author>();
+            List<Book> booksList = new List<Book>();
+
+            authorsList.Add(new Author(1,"Yazar1"));
+            authorsList.Add(new Author(2, "Yazar2"));
+            authorsList.Add(new Author(3, "Yazar3"));
+            booksList.Add(new Book(1, 1, "Kitap1"));
+            booksList.Add(new Book(1, 2, "Kitap2"));
+            booksList.Add(new Book(1, 3, "Kitap3"));
+            booksList.Add(new Book(1, 4, "Kitap4"));
+            booksList.Add(new Book(2, 5, "Kitap1"));
+            booksList.Add(new Book(2, 6, "Kitap2"));
+            booksList.Add(new Book(3, 7, "Kitap1"));
+
+            foreach (var values in booksList.Join(authorsList, booksListed => booksListed.authorId, authorsList2 => authorsList2.authorId, (booksListed, authorlist2) => new {bookName= booksListed.bookTitle,authorName=authorlist2.authorName}))
+            {
+                Console.WriteLine("Yazar Adi:"+values.authorName + "\t Kitap Adi:"+values.bookName);
+            }
+            
+
+        }
+    }
+}
+
