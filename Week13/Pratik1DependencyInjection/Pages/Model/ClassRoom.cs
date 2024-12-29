60ad580bfa40cc04dd5bae89a7c345572898d7e5
Atakann <53114980+algatrazcpt@users.noreply.github.com>
2024-12-29 12:39:15 +0300

    week13 Pratik1DepecnyInjection

---
+++ Week13/Pratik1DependencyInjection/Pages/Model/ClassRoom.cs
@@ -0,0 +1,17 @@
+﻿namespace Pratik1DependencyInjection.Pages.Model
+{
+    public class ClassRoom
+    {
+        private ITeacher teacher;
+
+        public ClassRoom (ITeacher teacher)
+        {
+            this.teacher = teacher;
+        }
+        public void StartClass()
+        {
+            Console.WriteLine("Class Started");
+            teacher.GetInfo();
+        }
+    }
+}
---
+++ Week13/Pratik1DependencyInjection/Pages/Model/Teacher.cs
@@ -0,0 +1,18 @@
+﻿namespace Pratik1DependencyInjection.Pages.Model
+{
+    public interface ITeacher
+    {
+        public string GetInfo();
+    }
+
+    public class Teacher:ITeacher
+    {
+        string firstName = "İlk Ad";
+        string lastName = "Soyad";
+
+        public string GetInfo()
+        {
+            return firstName + "-" + lastName;
+        }
+    }
+}
